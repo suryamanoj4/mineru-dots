bdd45bf0590a5bbe64874f01fbf403255d600839
surya <suryamanojphy31@gmail.com>
2026-03-27 18:20:47 +0530

    Update VLM model path to dots.ocr (rednote-hilab/dots.mocr)

--- mineru/utils/enum_class.py
+++ mineru/utils/enum_class.py
@@ -1,20 +1,21 @@
 from enum import Enum
 
+
 class BlockType:
-    IMAGE = 'image'
-    TABLE = 'table'
-    IMAGE_BODY = 'image_body'
-    TABLE_BODY = 'table_body'
-    IMAGE_CAPTION = 'image_caption'
-    TABLE_CAPTION = 'table_caption'
-    IMAGE_FOOTNOTE = 'image_footnote'
-    TABLE_FOOTNOTE = 'table_footnote'
-    TEXT = 'text'
-    TITLE = 'title'
-    INTERLINE_EQUATION = 'interline_equation'
-    LIST = 'list'
-    INDEX = 'index'
-    DISCARDED = 'discarded'
+    IMAGE = "image"
+    TABLE = "table"
+    IMAGE_BODY = "image_body"
+    TABLE_BODY = "table_body"
+    IMAGE_CAPTION = "image_caption"
+    TABLE_CAPTION = "table_caption"
+    IMAGE_FOOTNOTE = "image_footnote"
+    TABLE_FOOTNOTE = "table_footnote"
+    TEXT = "text"
+    TITLE = "title"
+    INTERLINE_EQUATION = "interline_equation"
+    LIST = "list"
+    INDEX = "index"
+    DISCARDED = "discarded"
 
     # Added in vlm 2.5
     CODE = "code"
@@ -31,33 +32,33 @@ class BlockType:
 
 
 class ContentType:
-    IMAGE = 'image'
-    TABLE = 'table'
-    TEXT = 'text'
-    INTERLINE_EQUATION = 'interline_equation'
-    INLINE_EQUATION = 'inline_equation'
-    EQUATION = 'equation'
-    CODE = 'code'
+    IMAGE = "image"
+    TABLE = "table"
+    TEXT = "text"
+    INTERLINE_EQUATION = "interline_equation"
+    INLINE_EQUATION = "inline_equation"
+    EQUATION = "equation"
+    CODE = "code"
 
 
 class ContentTypeV2:
-    CODE = 'code'
+    CODE = "code"
     ALGORITHM = "algorithm"
-    EQUATION_INTERLINE = 'equation_interline'
-    IMAGE = 'image'
-    TABLE = 'table'
-    TABLE_SIMPLE = 'simple_table'
-    TABLE_COMPLEX = 'complex_table'
-    LIST = 'list'
-    LIST_TEXT = 'text_list'
-    LIST_REF = 'reference_list'
-    TITLE = 'title'
-    PARAGRAPH = 'paragraph'
-    SPAN_TEXT = 'text'
-    SPAN_EQUATION_INLINE = 'equation_inline'
-    SPAN_PHONETIC = 'phonetic'
-    SPAN_MD = 'md'
-    SPAN_CODE_INLINE = 'code_inline'
+    EQUATION_INTERLINE = "equation_interline"
+    IMAGE = "image"
+    TABLE = "table"
+    TABLE_SIMPLE = "simple_table"
+    TABLE_COMPLEX = "complex_table"
+    LIST = "list"
+    LIST_TEXT = "text_list"
+    LIST_REF = "reference_list"
+    TITLE = "title"
+    PARAGRAPH = "paragraph"
+    SPAN_TEXT = "text"
+    SPAN_EQUATION_INLINE = "equation_inline"
+    SPAN_PHONETIC = "phonetic"
+    SPAN_MD = "md"
+    SPAN_CODE_INLINE = "code_inline"
     PAGE_HEADER = "page_header"
     PAGE_FOOTER = "page_footer"
     PAGE_NUMBER = "page_number"
@@ -84,18 +85,20 @@ class CategoryId:
 
 
 class MakeMode:
-    MM_MD = 'mm_markdown'
-    NLP_MD = 'nlp_markdown'
-    CONTENT_LIST = 'content_list'
-    CONTENT_LIST_V2 = 'content_list_v2'
+    MM_MD = "mm_markdown"
+    NLP_MD = "nlp_markdown"
+    CONTENT_LIST = "content_list"
+    CONTENT_LIST_V2 = "content_list_v2"
 
 
 class ModelPath:
-    vlm_root_hf = "opendatalab/MinerU2.5-2509-1.2B"
-    vlm_root_modelscope = "OpenDataLab/MinerU2.5-2509-1.2B"
+    vlm_root_hf = "rednote-hilab/dots.mocr"
+    vlm_root_modelscope = "rednote-hilab/dots.ocr"
     pipeline_root_modelscope = "OpenDataLab/PDF-Extract-Kit-1.0"
     pipeline_root_hf = "opendatalab/PDF-Extract-Kit-1.0"
-    doclayout_yolo = "models/Layout/YOLO/doclayout_yolo_docstructbench_imgsz1280_2501.pt"
+    doclayout_yolo = (
+        "models/Layout/YOLO/doclayout_yolo_docstructbench_imgsz1280_2501.pt"
+    )
     yolo_v8_mfd = "models/MFD/YOLO/yolo_v8_ft.pt"
     unimernet_small = "models/MFR/unimernet_hf_small_2503"
     pp_formulanet_plus_m = "models/MFR/pp_formulanet_plus_m"
@@ -104,17 +107,19 @@ class ModelPath:
     slanet_plus = "models/TabRec/SlanetPlus/slanet-plus.onnx"
     unet_structure = "models/TabRec/UnetStructure/unet.onnx"
     paddle_table_cls = "models/TabCls/paddle_table_cls/PP-LCNet_x1_0_table_cls.onnx"
-    paddle_orientation_classification = "models/OriCls/paddle_orientation_classification/PP-LCNet_x1_0_doc_ori.onnx"
+    paddle_orientation_classification = (
+        "models/OriCls/paddle_orientation_classification/PP-LCNet_x1_0_doc_ori.onnx"
+    )
 
 
 class SplitFlag:
-    CROSS_PAGE = 'cross_page'
-    LINES_DELETED = 'lines_deleted'
+    CROSS_PAGE = "cross_page"
+    LINES_DELETED = "lines_deleted"
 
 
 class ImageType:
-    PIL = 'pil_img'
-    BASE64 = 'base64_img'
+    PIL = "pil_img"
+    BASE64 = "base64_img"
 
 
 class NotExtractType(Enum):
