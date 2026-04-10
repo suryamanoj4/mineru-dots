c618abfe205d5c0feb545e05ee208ed3a08b77fb
LagichettyKushal <lagichettykushal@gmail.com>
2026-04-10 11:53:38 +0530

    feat: add base tesseract OCR wrapper for pipeline-lite groundwork

--- mineru/model/ocr/__init__.py
+++ mineru/model/ocr/__init__.py
@@ -1,1 +1,9 @@
 # Copyright (c) Opendatalab. All rights reserved.
+
+from .pytorch_paddle import PytorchPaddleOCR
+from .tesseract import TesseractOCRModel
+
+__all__ = [
+    "PytorchPaddleOCR",
+    "TesseractOCRModel",
+]
