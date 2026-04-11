6111ffbb50896e13d64a44fb6fdc22840b2ccff7
Lakshy Yarlagadda <lakshyyarlagadda1@gmail.com>
2026-04-11 16:01:37 +0530

    chore: remove unused helpers and dead data/table modules

--- mineru/backend/vlm/dots_ocr/utils/consts.py
+++
@@ -1,4 +0,0 @@
-MIN_PIXELS = 3136
-MAX_PIXELS = 11289600
-
-image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".tif", ".webp"]
--- mineru/data/data_reader_writer/filebase.py
+++ mineru/data/data_reader_writer/filebase.py
@@ -1,38 +1,6 @@
 import os
 
-from .base import DataReader, DataWriter
-
-
-class FileBasedDataReader(DataReader):
-    def __init__(self, parent_dir: str = ''):
-        """Initialized with parent_dir.
-
-        Args:
-            parent_dir (str, optional): the parent directory that may be used within methods. Defaults to ''.
-        """
-        self._parent_dir = parent_dir
-
-    def read_at(self, path: str, offset: int = 0, limit: int = -1) -> bytes:
-        """Read at offset and limit.
-
-        Args:
-            path (str): the path of file, if the path is relative path, it will be joined with parent_dir.
-            offset (int, optional): the number of bytes skipped. Defaults to 0.
-            limit (int, optional): the length of bytes want to read. Defaults to -1.
-
-        Returns:
-            bytes: the content of file
-        """
-        fn_path = path
-        if not os.path.isabs(fn_path) and len(self._parent_dir) > 0:
-            fn_path = os.path.join(self._parent_dir, path)
-
-        with open(fn_path, 'rb') as f:
-            f.seek(offset)
-            if limit == -1:
-                return f.read()
-            else:
-                return f.read(limit)
+from .base import DataWriter
 
 
 class FileBasedDataWriter(DataWriter):
